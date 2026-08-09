4e4e8937590a8b700963598c368ff7aae8a7aba1
AtikHasanSarker <atiksagor24@gmail.com>
2026-08-09 13:50:21 +0600

    Created basic schema and routes

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+// import router from "./routes";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hello, World!",
+  });
+});
+
+export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+// import products from "../services/products";
+// import orders from "../services/order";
+
+const router = Router();
+// router.use("/products", products);
+// router.use("/orders", orders);
+
+export default router;
